1915f08c9d050d7c5c95191d62facde21aae7edb
JQZ1111 <jiaqi.zhao@polymtl.ca>
2025-04-12 15:23:58 -0400

    Made the VUE module for the chessboard. No interaction yet avec la partie logique

--- chess_project/mainwindow.cpp
+++ chess_project/mainwindow.cpp
@@ -6,9 +6,38 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    layout_ = new QGridLayout();
+    layout_->setSpacing(0);
+    layout_->setContentsMargins(0, 0, 0, 0);
+
+    echequier_ = new QWidget();
+    for(int i = 0; i < TAILLEECHEQUIER; i++){
+        for(int j = 0; j<TAILLEECHEQUIER; j++){
+            buttons_[(i*TAILLEECHEQUIER)+j] = new QPushButton();
+            buttons_[(i*TAILLEECHEQUIER)+j]->setFixedHeight(40);
+            buttons_[(i*TAILLEECHEQUIER)+j]->setFixedWidth(40);
+            if ((i+j)%2 == 1){
+                buttons_[(i*TAILLEECHEQUIER)+j]->setStyleSheet("background-color:yellow;");
+            }
+            else buttons_[(i*TAILLEECHEQUIER)+j]->setStyleSheet("background-color:brown;");
+            layout_->addWidget(buttons_[(i*TAILLEECHEQUIER)+j], i, j);
+        }
+    }
+    echequier_->setLayout(layout_);
+    echequier_->setMaximumHeight(40*TAILLEECHEQUIER);
+    echequier_->setMaximumWidth(40*TAILLEECHEQUIER);
+    setCentralWidget(echequier_);
+    // Cant center it tho
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete echequier_;
+    delete layout_;
+    for(int i = 0; i<TAILLEECHEQUIER*TAILLEECHEQUIER; i++){
+        delete buttons_[i];
+    }
 }
+
--- chess_project/mainwindow.h
+++ chess_project/mainwindow.h
@@ -2,9 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QWidget>
+
 /*
  * Module pour la Vue et le Controlleur du modèle d'interface graphique
 **/
+
+int const TAILLEECHEQUIER = 8;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,5 +28,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QGridLayout* layout_;
+    QWidget* echequier_;
+    QPushButton* buttons_[TAILLEECHEQUIER*TAILLEECHEQUIER];
 };
 #endif // MAINWINDOW_H
+
