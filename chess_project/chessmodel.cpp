2e0bc69d88cb690bc2115d97487da077e6b36108
JQZ1111 <jiaqi.zhao@polymtl.ca>
2025-04-13 17:55:09 -0400

    Put the headers for everything except for presence of Qt modules inside the logical part of the gui

--- chess_project/chessmodel.cpp
+++ chess_project/chessmodel.cpp
@@ -1,3 +1,76 @@
 #include "chessmodel.h"
 
-chessmodel::chessmodel() {}
+/*
+Chessmodel::Chessmodel(){
+
+};
+*/
+
+Emplacement::Emplacement():horizontalPos_('a'), verticalPos_(0){};
+
+Emplacement::Emplacement(char horizontalPos, int verticalPos):horizontalPos_(horizontalPos), verticalPos_(verticalPos){};
+
+int Emplacement::convertCharToInt(){
+    return int(horizontalPos_) - int('a');
+}
+
+Piece::Piece():position_(),isBlack_(true){};
+
+Piece::Piece(Emplacement position, bool isBlack):position_(position), isBlack_(isBlack){};
+
+void Piece::changerPosition(Emplacement nouvellePosition){
+    position_ = nouvellePosition;
+}
+
+Emplacement Piece::getEmplacement() const {return position_;}
+
+int Roi::nbrRois_ = 0;
+
+Roi::Roi():Piece(){nbrRois_+=1;};
+
+Roi::Roi(Emplacement position, bool isBlack):Piece(position, isBlack){nbrRois_+=1;};
+
+bool Roi::canMoveTo(Emplacement nouvellePosition){
+    if (abs(nouvellePosition.verticalPos_ - position_.horizontalPos_)<=1 && abs(nouvellePosition.convertCharToInt() - position_.convertCharToInt())<=1)
+        return true;
+    else
+        return false;
+};
+
+Tour::Tour():Piece(){};
+
+Tour::Tour(Emplacement position, bool isBlack):Piece(position, isBlack){};
+
+bool Tour::canMoveTo(Emplacement nouvellePosition){
+    if (abs(position_.verticalPos_ - nouvellePosition.verticalPos_) == 0 || abs(position_.convertCharToInt() - nouvellePosition.convertCharToInt()) == 0)
+        return true;
+    else
+        return false;
+}
+
+Cavalier::Cavalier():Piece(){};
+
+Cavalier::Cavalier(Emplacement position, bool isBlack): Piece(position, isBlack){};
+
+bool Cavalier::canMoveTo(Emplacement nouvelleEmplacement){
+    if (abs(position_.verticalPos_ - nouvelleEmplacement.verticalPos_) == 3 && abs(position_.convertCharToInt() - nouvelleEmplacement.convertCharToInt())==2)
+        return true;
+    else if (abs(position_.verticalPos_ - nouvelleEmplacement.verticalPos_) == 2 && abs(position_.convertCharToInt() - nouvelleEmplacement.convertCharToInt())==3)
+        return true;
+    else
+        return false;
+}
+
+Echequier::Echequier(){};
+
+Echequier::~Echequier(){};
+
+void Echequier::placerPiece(Piece& pieceAPlacer, Emplacement emplacementAPlacer){
+
+}
+
+void Echequier::deplacer(Piece& pieceADeplacer, Emplacement emplacementFinale){
+    pieceADeplacer.changerPosition(emplacementFinale);
+}
+
+
--- chess_project/chessmodel.h
+++ chess_project/chessmodel.h
@@ -1,13 +1,112 @@
 #ifndef CHESSMODEL_H
 #define CHESSMODEL_H
+#include <QObject>
+#include <memory>
+#include <vector>
 
 /*
  * Module pour faire le modèle Logique du jeu
 **/
-class chessmodel
+
+
+
+namespace logic{
+//class Chessmodel;
+struct Emplacement;
+class Piece;
+class Roi;
+class Tour;
+class Cavalier;
+class Echequier;
+//class Move;
+}
+/*
+class Chessmodel
 {
 public:
-    chessmodel();
+    Chessmodel();
 };
+*/
+
+struct Emplacement{
+    Emplacement();
+    Emplacement(char horizontalPos, int verticalPos);
+    int convertCharToInt();
+    char horizontalPos_;
+    int verticalPos_;
+};
+
+class Piece{
+    // Si tu veux mettre le Q_OBJECT, faut absolument que la classe hérite d'une classe commençant par Q (QObject par exemple)
+    Q_OBJECT
+public:
+    Piece();
+    Piece(Emplacement position, bool isBlack);
+    void changerPosition(Emplacement nouvellePosition);
+    Emplacement getEmplacement() const;
+protected:
+    Emplacement position_;
+    bool isBlack_;
+};
+
+class Roi: public Piece{
+    Q_OBJECT
+public:
+    Roi();
+    Roi(Emplacement position, bool isBlack);
+    bool canMoveTo(Emplacement nouvellePosition);
+protected:
+    static int nbrRois_;
+};
+
+class Tour:public Piece{
+    Q_OBJECT
+public:
+    Tour();
+    Tour(Emplacement position, bool isBlack);
+    bool canMoveTo(Emplacement nouvellePosition);
+};
+
+class Cavalier:public Piece{
+    Q_OBJECT
+public:
+    Cavalier();
+    Cavalier(Emplacement position, bool isBlack);
+    bool canMoveTo(Emplacement nouvellePosition);
+};
+
+class Echequier{
+    Q_OBJECT
+public:
+    Echequier();
+    ~Echequier();
+    void placerPiece(Piece& pieceAPlacer, Emplacement emplacementAPlacer);
+    void deplacer(Piece& pieceADeplacer, Emplacement emplacementFinale);
+private:
+    std::vector<std::unique_ptr<Roi>> rois_;
+    std::vector<std::unique_ptr<Tour>> tours_;
+    std::vector<std::unique_ptr<Cavalier>> cavaliers_;
+signals:
+    // Signal qu'on assigne une position à une certaine pièce
+    void assignerPosition();
+    // Signal qu'on enlève une pièce d'une certaine position
+    void enleverDeLaPosition();
+};
+/*
+class Move{
+public:
+    // Ne devrait pas avoir de constructeur par défaut, car on crée la class seulement pour savoir si on bouge ou pas
+    Move(Echequier& echequier, Piece& pieceABouger, Emplacement& positionFinale);
+
+    void bouger();
+private:
+    Echequier& echequier_;
+    Piece& pieceABouger_;
+    Emplacement& positionFinale_;
+signals:
+};
+*/
+
+
 
 #endif // CHESSMODEL_H
